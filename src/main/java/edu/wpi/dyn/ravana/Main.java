5e70dacd7fbf022a2fcf9c88e2b310ff068568e0
Sourec <tothemyers@gmail.com>
2019-04-20 15:58:15 -0400

    Add log4j support

--- src/main/java/edu/wpi/dyn/ravana/Main.java
+++ src/main/java/edu/wpi/dyn/ravana/Main.java
@@ -19,8 +19,14 @@
 
 package edu.wpi.dyn.ravana;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Main {
+
+	private static final Logger logger = LogManager.getLogger();
+
 	public static void main(String[] args) {
-		System.out.println("Hello, world!");
+		logger.info("Hello, world!");
 	}
 }
